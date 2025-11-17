b8789542650af6958b09097cd20e14bf4c3717cb
Emanuel Oliveira Andrade <101219040+emanuel-o-dev@users.noreply.github.com>
2025-11-17 17:27:21 -0300

    @emanuelodev/fix/db and routes (#7)
    * Add migration for order_items table and update web routes
    
    * Update LegoTech and remove outdated pages

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,9 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-// Initial Page
 Route::get('/', function () {
-    return view('public.auth');
-})->name('auth');
-
-// Authentication Routes
-Route::post('/auth', [LoginController::class, 'auth']);
-
-// Public Routes
-Route::view('/policies', 'public.policies')->name('policies');
-Route::view('/terms', 'public.terms')->name('terms');
-Route::view('/about', 'public.about')->name('about');
-Route::view('/contact', 'public.contact')->name('contact');
-
+    return view('welcome');
+});
 Route::fallback(function () {
     return response()->view('errors.generic', [
         'code' => 404,
