4327fb8c2eb3e9aed346049a42193bc71cadc224
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-11-17 16:27:39 -0300

    Remove unnecessary blank lines in Category, Order, and Product model files

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -23,4 +23,3 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 }
-
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -32,4 +32,3 @@ class Product extends Model
         return $this->hasMany(OrderItem::class);
     }
 }
-
