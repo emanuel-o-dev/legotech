fe2d3267ea72fdaf1f2a1ab0eda32a1e84fc60a4
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-11-17 16:21:28 -0300

    Enhance model relationships and fillable attributes for Category, Order, OrderItem, Product, and User

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -45,5 +46,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relation with Order
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'ADMIN';
     }
 }
