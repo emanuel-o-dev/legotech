163338f3f8f43dde7ca0b84a83ca458137d35954
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-11-18 15:47:28 -0300

    add toast messages for success and error notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,23 @@
         </div>
     </footer>
 
+    <!-- TOAST MESSAGES -->
+    @if (session('success'))
+        <div class="toast toast-top toast-end z-50">
+            <div class="alert alert-success">
+                <span>{{ session('success') }}</span>
+            </div>
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="toast toast-top toast-end z-50">
+            <div class="alert alert-error">
+                <span>{{ session('error') }}</span>
+            </div>
+        </div>
+    @endif
+
 </body>
 
 </html>
