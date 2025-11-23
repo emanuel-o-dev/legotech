a8ac2d956045bcf9b8e1337c42a0b1dcedbffd5e
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-11-23 20:55:35 -0300

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    {{-- icon --}}
+    <link rel="icon" href="{{ asset('/images/lego.png') }}">
+    {{-- Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     <title>@yield('title', 'LegoTech')</title>
 </head>
 
